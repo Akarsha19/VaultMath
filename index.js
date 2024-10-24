efda450257fce5a80fdde40a6e3f29e1b987fa52
Akarsha19 <akarshap19@gmail.com>
2024-10-24 01:58:33 -0400

    finished commit

---
+++ index.js
@@ -0,0 +1,10 @@
+//message to user of secret combination
+alert(`You have received this message because you have been chosen to open an important vault. Here is the secret combination:`);
+//use add, subtract, multiply 
+const codeAdd = (5 + 5);
+
+const codeSubtract = (45 - 5);
+
+const codeMultiply = (3 * 13);
+//bring variables into alert ($)
+alert(`The combination is ${codeAdd} - ${codeSubtract} - ${codeMultiply}`);
